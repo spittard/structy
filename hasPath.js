fbe09807c8a9a46be1654deca55273de9886a660
spittard <scott.pittard@gmail.com>
2023-03-17 09:10:25 -0500

    first commit

---
+++ hasPath.js
@@ -0,0 +1,28 @@
+const hasPath = (graph, src, dst) => {
+  
+    const queue = [src]
+    
+    while(queue.length > 0){
+      const current = queue.shift()
+      
+      if(current === dst) return true
+      
+      for(let neighbor of graph[current])
+        queue.push(neighbor)
+    } 
+    
+    return false
+      
+  };
+  
+  const graph = {
+    f: ['g', 'i'],
+    g: ['h'],
+    h: [],
+    i: ['g', 'k'],
+    j: ['i'],
+    k: []
+  };
+  
+  hasPath(graph, 'f', 'k'); 
+  
